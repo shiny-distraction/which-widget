3d44de5fe25f568bbe2d9061839643550848f359
Dylan Smith <dylantsmith@gmail.com>
2012-11-05 18:47:54 -0600

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WhichWidget::Application.config.session_store :cookie_store, key: '_which-widget_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WhichWidget::Application.config.session_store :active_record_store
