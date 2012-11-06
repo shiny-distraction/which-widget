d5fa18f9ea5fa5188381975abd0bdef917ce4677
Dylan Smith <dylantsmith@gmail.com>
2012-11-05 19:32:12 -0600

    add welcome controller for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 WhichWidget::Application.routes.draw do
+  get "welcome/index"
+
   match 'weather/forecast/:state/:city' => "weather#forecast"
   # http://localhost:3000/weather/conditions/TX/Austin.json?q=temp_f
   match 'weather/conditions/:state/:city' => "weather#conditions"
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 end
